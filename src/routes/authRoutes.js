813e2a4b41ae5c8ecaa044c3be00b07c55a8d255
MartinJara2401 <martin.jara2401@alumnos.ubiobio.cl>
2026-06-09 10:42:46 -0400

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {validationResult}= require('express-validator');
-const verificarToken= require('../middlewares/verificarToken');
+const verificarToken= require('../auth.mjs/verificarToken');
 const ctrl= require('../controllers/authController');
 const v = require('../validations/authValidations');
 //middleware va a reutilizar para hacer un reporte de errores de validacion
